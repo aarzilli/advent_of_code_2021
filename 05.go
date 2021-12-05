f705ee65f8eed15fe7b6bae947cc821106881d7c
aarzilli <alessandro.arzilli@gmail.com>
2021-12-05 07:38:17 +0100

    day 5 cleanup

--- 05.go
+++ 05.go
@@ -13,70 +13,56 @@ type Point struct {
 	x, y int
 }
 
-const dopart2 = true
+func sign(x int) int {
+	if x == 0 {
+		return 0
+	}
+	if x < 0 {
+		return -1
+	}
+	return 1
+}
 
-var M = map[Point]int{}
+func trace(M map[Point]int, start, end []int) {
+	deltax := sign(end[0] - start[0])
+	deltay := sign(end[1] - start[1])
+	x, y := start[0], start[1]
+	for {
+		M[Point{x, y}]++
+		if x == end[0] && y == end[1] {
+			break
+		}
+		x, y = x+deltax, y+deltay
+	}
+}
+
+func overlaps(M map[Point]int) int {
+	r := 0
+	for _, v := range M {
+		if v > 1 {
+			r++
+		}
+	}
+	return r
+}
+
+const dopart2 = true
 
 func main() {
 	lines := Input("05.txt", "\n", true)
-	pf("len %d\n", len(lines))
+	var M1 = map[Point]int{}
+	var M2 = map[Point]int{}
+
 	for _, line := range lines {
 		v := Spac(line, "->", 2)
-		coord0 := Vatoi(Spac(v[0], ",", 2))
-		coord1 := Vatoi(Spac(v[1], ",", 2))
-		//pf("%v %v\n", coord0, coord1)
-		if coord0[0] == coord1[0] {
-			x := coord0[0]
-			starty := Min([]int{coord0[1], coord1[1]})
-			endy := Max([]int{coord0[1], coord1[1]})
-			for y := starty; y <= endy; y++ {
-				M[Point{x, y}]++
-			}
-		} else if coord0[1] == coord1[1] {
-			y := coord0[1]
-			startx := Min([]int{coord0[0], coord1[0]})
-			endx := Max([]int{coord0[0], coord1[0]})
-			for x := startx; x <= endx; x++ {
-				M[Point{x, y}]++
-			}
-		} else if dopart2 {
-			deltax := 1
-			if coord0[0] > coord1[0] {
-				deltax = -1
-			}
-			deltay := 1
-			if coord0[1] > coord1[1] {
-				deltay = -1
-			}
-			//pf("%v %v\n", coord0, coord1)
-			x, y := coord0[0], coord0[1]
-			for {
-				//pf("\t%d %d\n", x, y)
-				M[Point{x, y}]++
-				if x == coord1[0] && y == coord1[1] {
-					break
-				}
-				x, y = x+deltax, y+deltay
-			}
+		start := Vatoi(Spac(v[0], ",", 2))
+		end := Vatoi(Spac(v[1], ",", 2))
+		if start[0] == end[0] || start[1] == end[1] {
+			trace(M1, start, end)
 		}
+		trace(M2, start, end)
 	}
 
-	/*
-		for y := 0; y <= 9; y++ {
-			for x := 0; x <= 9; x++ {
-				pf("%d", M[Point{x,y}])
-			}
-			pf("\n")
-		}
-	*/
-
-	part1 := 0
-	for _, v := range M {
-		if v > 1 {
-			part1++
-		}
-	}
-	Sol(part1)
+	Sol(overlaps(M1))
+	Sol(overlaps(M2))
 }
-
-// 21554
