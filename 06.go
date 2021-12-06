62ca222bace406149e6ab00249a8815f7a637104
aarzilli <alessandro.arzilli@gmail.com>
2021-12-06 08:37:07 +0100

    day 6 cleanup

--- 06.go
+++ 06.go
@@ -9,32 +9,19 @@ func pf(fmtstr string, any ...interface{}) {
 	fmt.Printf(fmtstr, any...)
 }
 
-var fishes []int
-var fm = map[int]int{}
-
-func step() {
-	for i := range fishes {
-		if fishes[i] == 0 {
-			fishes[i] = 6
-			fishes = append(fishes, 8)
-		} else {
-			fishes[i]--
-		}
-	}
-}
+var fm [9]int
 
 func mstep() {
-	nfm := map[int]int{}
+	var nfm [9]int
 	for k, v := range fm {
 		if k == 0 {
-			k = 6
 			nfm[8] += v
-			nfm[k] += v
+			nfm[6] += v
 		} else {
 			nfm[k-1] += v
 		}
 	}
-	fm = nfm
+	copy(fm[:], nfm[:])
 }
 
 func mcount() int {
@@ -47,25 +34,15 @@ func mcount() int {
 
 func main() {
 	lines := Input("06.txt", "\n", true)
-	pf("len %d\n", len(lines))
-	fishes = Vatoi(Spac(lines[0], ",", -1))
+	fishes := Vatoi(Spac(lines[0], ",", -1))
 	for _, fish := range fishes {
 		fm[fish]++
 	}
 	for i := 0; i < 256; i++ {
-		pf("day %d: %d %d\n", i, len(fishes), mcount())
-		/*if len(fishes) != mcount() {
-			panic("blah")
-		}*/
-		//step()
+		if i == 80 {
+			Sol(mcount())
+		}
 		mstep()
-		//pf("after %d: %v\n", i+1, fishes)
 	}
 	Sol(mcount())
 }
-
-// after 2: [1 2 1 6 0 8]
-// After  2 days: 1,2,1,6,0,8
-
-// 6 0 6 4 5 6 0 1 1 2 6 0 1 1 1 2 2 3 3 4 6 7 8 8 8 8
-// 6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8
