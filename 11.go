22cb8741b4db0c073a7754a7bd7cdb8f4b9b9579
aarzilli <alessandro.arzilli@gmail.com>
2021-12-11 12:57:53 +0100

    day 11 cleanup

--- 11.go
+++ 11.go
@@ -13,20 +13,6 @@ var M [][]int
 var totalflashes int
 var magicflash bool
 
-func printmatrix() {
-	for i := range M {
-		for j := range M[i] {
-			if M[i][j] > 9 {
-				pf("X")
-			} else {
-				pf("%d", M[i][j])
-			}
-		}
-		pf("\n")
-	}
-	pf("\n")
-}
-
 func step() {
 	for i := range M {
 		for j := range M[i] {
@@ -34,12 +20,9 @@ func step() {
 		}
 	}
 
-	// flashing
 	n := numflashing()
 	flashed := map[[2]int]bool{}
 	for {
-		//pf("flashing\n")
-		//printmatrix()
 		flashonce(flashed)
 		n2 := numflashing()
 		if n2 == n {
@@ -55,11 +38,8 @@ func step() {
 
 func flashonce(flashed map[[2]int]bool) {
 	toflash := [][2]int{}
-	//M2 := make([][]int, len(M))
 	for i := range M {
-		//M2[i] = make([]int, len(M[i])
 		for j := range M[i] {
-			//M2[i][j] = M[i]
 			if M[i][j] > 9 && !flashed[[2]int{i, j}] {
 				toflash = append(toflash, [2]int{i, j})
 			}
@@ -114,7 +94,6 @@ func resetflashed() {
 
 func main() {
 	lines := Input("11.txt", "\n", true)
-	pf("len %d\n", len(lines))
 
 	for i, line := range lines {
 		M = append(M, make([]int, len(line)))
@@ -123,12 +102,8 @@ func main() {
 		}
 	}
 
-	printmatrix()
-
 	for i := 1; i <= 1000; i++ {
-		//pf("step %d:\n", i)
 		step()
-		//printmatrix()
 		resetflashed()
 		if i == 100 {
 			Sol(totalflashes)
